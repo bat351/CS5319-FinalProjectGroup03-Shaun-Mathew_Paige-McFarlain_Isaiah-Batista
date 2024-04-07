4a821effe6a1a45b8c93f9375952e61683afe50f
Shaun Mathew <shaunsanthosh@gmail.com>
2024-04-07 15:49:55 -0500

    Create data.py

---
+++ Unselected/data.py
@@ -0,0 +1,28 @@
+import pygame, sys
+
+# ModelCe
+SCREEN = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("assets/Background.png")
+
+CHECKERS_WINS = [0, 0]
+CONNECT_4_WINS = [0, 0]
+TIC_TAC_TOE_WINS = [0, 0]
+
+CURRENT_PLAYER = 'Player 1'
+
+
+def change_player():
+  global CURRENT_PLAYER
+  if CURRENT_PLAYER == 'Player 1':
+    CURRENT_PLAYER = 'Player 2'
+  else:
+    CURRENT_PLAYER = 'Player 1'
+
+def get_player():
+  return CURRENT_PLAYER;
+
+def reset_player():
+  global CURRENT_PLAYER
+  CURRENT_PLAYER = 'Player 1'
